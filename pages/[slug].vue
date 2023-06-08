3ea32bafea5df315a04c48e41d371ae93a8e1730
Ian Febi Sastrataruna <IAN463@PID-PROJECT-IAN643.local>
2023-06-08 10:58:35 +0700

    gas

---
+++ pages/[slug].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const route = useRoute();
+const params = ref<string>(route.query.name as string);
+</script>
+
+<template>
+  <div>
+    <span>Params:</span>
+    <div>{{ params }}</div>
+  </div>
+</template>
